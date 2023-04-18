24de6ca1d71f9f041c2f151c9a80117beb849fe0
inkook97 <127088267+inkook97@users.noreply.github.com>
2023-04-18 12:45:19 +0900

    0418_node

--- router/module1.js
+++ router/module1.js
@@ -37,6 +37,7 @@ module.exports = (app, fs) => {
         });
     });
 
+    // 회원가입 
     // http://localhost:3000/joinMember/apple1 추가
     app.post('/joinMember/:userid', (req, res) => {
         const result = {}; // 실제로 데이터를 입력 요소가 수정되는건 상관없음, 데이터의 상태를 저장하는 공간
@@ -78,8 +79,57 @@ module.exports = (app, fs) => {
 
     // 회원수정
     // http://localhost:3000/updateMember/apple1
+    app.put('/updateMember/:userid', (req, res) => {
+        const result = {};
+        const userid = req.params.userid; // 요청한 변수의 값을 userid라는 공간에 지정
 
+        if (!req.body["password"] || !req.body["name"]) {
+            result["success"] = 100;
+            result["msg"] = "매개변수가 전달되지 않음";
+            res.json(result);
+            return false;
+        }
+
+        // 저장된 데이터를 읽기
+        fs.readFile(__dirname + "/../data/member.json", "utf-8", (err, data) => {
+            if (!err) {
+                // 데이터 쓰기
+                const member = JSON.parse(data); // JSON파일로 저장
+                member[userid] = req.body; // 전달한 정보 name, password
+                fs.writeFile(__dirname + "/../data/member.json", JSON.stringify(member, null, '\t'), 'utf-8', (err, data) => {
+                    if (!err) {
+                        result["success"] = 200;
+                        result["msg"] = "성공";
+                        res.json(result);
+                    } else {
+                        console.log(err);
+                    };
+                });
+            } else {
+                console.log(err);
+            };
+        });
+    });
 
     // 회원정보 삭제
-    // http://localhost:3000/deletMember/berry
+    // http://localhost:3000/deleteMember/apple1
+    app.delete('/deleteMember/:userid', (req, res) => {
+        let result = {};
+        fs.readFile(__dirname + '/../data/member.json', 'utf-8', (err, data) => {
+            const member = JSON.parse(data);
+            // 삭제할 데이터가 있는지 확인
+            if (!member[req.params.userid]) {
+                result["success"] = 102;
+                result["msg"] = "사용자를 찾을 수 없음";
+                res.json(result);
+                return false;
+            }
+            delete member[req.params.userid]; // 데이터 삭제
+            fs.writeFile(__dirname + '/../data/member.json', JSON.stringify(member, null, '\t'), 'utf-8', (err, data) => {
+                result["success"] = 200;
+                result["msg"] = "성공";
+                res.json(result);
+            });
+        });
+    });
 };
