c3018e720d2240d0eaec456be3cdd3ae8505f48d
inkook97 <127088267+inkook97@users.noreply.github.com>
2023-05-03 12:48:07 +0900

    0503react남양몰

---
+++ day0503/src/components/event.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Event() {
+    return (
+        <div>
+            <div className="subimg">event</div>
+            <div className="contents">
+                <div style={{
+                    margin: '0 auto',
+                    width: '1100px',
+                    height: '1000px',
+                    backgroundColor: '#ccc'
+                }}>contents</div>
+            </div>
+        </div>
+    )
+}
+export default Event;
---
+++ day0503/src/mainimg.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Mainimg() {
+    return (
+        <div className='subimg'>
+            <img src="/images/mainimg.jpg" width="100%" alt="" />
+        </div>
+    )
+}
+
+export default Mainimg;
